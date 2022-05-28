72dd94c5a4b705c5ddc10970df0edd7d913ebf2e
Alex <alex.rodrigues23997@gmail.com>
2022-05-28 10:28:06 -0300

    Projeto-base criado

---
+++ src/main/java/com/azold6/corejavastudies/CoreJavaStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.azold6.corejavastudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreJavaStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreJavaStudiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azold6/corejavastudies/CoreJavaStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azold6.corejavastudies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreJavaStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
